782fc8b87c8e80d36e6a988735aa74162347964d
Jorge Vélez <jorgeluisvelez@gmail.com>
2026-05-03 07:47:50 -0700

    changed version and yml

--- src/minidexed.cpp
+++ src/minidexed.cpp
@@ -272,7 +272,7 @@ CMiniDexed::~CMiniDexed (void)
 
 bool CMiniDexed::Initialize (void)
 {
-	LOGNOTE("CMiniDexed::Initialize called - jorge build .720");
+	LOGNOTE("CMiniDexed::Initialize called - jorge build v.720");
 	assert (m_pConfig);
 	assert (m_pSoundDevice);
 
